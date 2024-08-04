c02a2a3af6d048236ea8eb82ce849cd9e1eabdd9
clarky <clarky@monumental.io>
2024-08-04 18:19:00 -0400

    Add patrolling behaviour

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -19,6 +19,8 @@ public class EnemyController : EntityController
     public Behaviour CurrentBehaviour = Behaviour.StandUntilAggroRange;
 
     private Vector3Int _aggroStartCellPosition;
+    private int _patrolPathIndexTarget;
+    private bool _isPatrolPathTraversalForward = true;
 
     protected override void Start()
     {
@@ -80,8 +82,25 @@ public class EnemyController : EntityController
         Debug.DrawLine(transform.position, DungeonGridController.CellToWorldCentered(DungeonGridController.Player.CellPosition), colour);
     }
 
-    // TODO: refactor to base class EntityController for reuse (ex: knockback cases)
+    private void TargetNextPatrolPathPoint()
+    {
+        var pathPointCount = Stats.PatrolPath.Count;
+        _patrolPathIndexTarget = Mathf.Clamp(_patrolPathIndexTarget, 0, pathPointCount - 1);
 
+        if (Stats.PatrolPingPong)
+        {
+            if (_isPatrolPathTraversalForward && _patrolPathIndexTarget == pathPointCount - 1)
+            {
+                _isPatrolPathTraversalForward = false;
+            }
+            else if (!_isPatrolPathTraversalForward && _patrolPathIndexTarget == 0)
+            {
+                _isPatrolPathTraversalForward = true;
+            }
+        }
+
+        _patrolPathIndexTarget = (_patrolPathIndexTarget + (_isPatrolPathTraversalForward ? 1 : -1) + pathPointCount) % pathPointCount;
+    }
 
     private void DoMoveStep()
     {
@@ -90,7 +109,6 @@ public class EnemyController : EntityController
 
         switch (CurrentBehaviour)
         {
-            case Behaviour.PatrolUntilAggroRange: // TODO: implement different
             case Behaviour.StandUntilAggroRange:
                 if (hasSightAggro && Attitude == EnemyStats.Attitude.Hostile)
                 {
@@ -108,6 +126,23 @@ public class EnemyController : EntityController
                 else
                 {
                     MoveRandomDirection();
+                }
+                break;
+            case Behaviour.PatrolUntilAggroRange:
+                if (hasSightAggro && Attitude == EnemyStats.Attitude.Hostile)
+                {
+                    CurrentBehaviour = Behaviour.ChaseUntilOutOfRange;
+                    MoveToTarget(DungeonGridController.Player.CellPosition);
+                }
+                else if (Stats.PatrolPath.Count > 0)
+                {
+                    var targetCellPosition = Stats.PatrolPath[_patrolPathIndexTarget];
+                    if (CellPosition == targetCellPosition)
+                    {
+                        TargetNextPatrolPathPoint();
+                        targetCellPosition = Stats.PatrolPath[_patrolPathIndexTarget];
+                    }
+                    MoveToTarget(targetCellPosition);
                 }
                 break;
             case Behaviour.ChaseUntilOutOfRange:
@@ -146,5 +181,50 @@ public class EnemyController : EntityController
         }
 
         CommonStats.LastMovedTime = Time.time;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (Stats.PatrolPath.Count == 0)
+        {
+            return;
+        }
+
+        var startColor = new Color(1.0f, 1.0f, 0.0f, 0.5f);
+        var mainColor = Stats.PatrolPingPong ? new Color(1.0f, 0.0f, 0.5f, 0.5f) : new Color(0.0f, 1.0f, 1.0f, 0.5f);
+        Gizmos.color = startColor;
+
+        var cellSize = DungeonGridController.GridLayout.cellSize;
+        var radius = (cellSize.x + cellSize.y) * 0.25f; // 0.25 because divide by 2 for average and divide by 2 again for changing diameter to radius
+        Vector3 previousPoint = DungeonGridController.CellToWorldCentered(Stats.PatrolPath[^1]);
+
+        for (int i = 0; i < Stats.PatrolPath.Count; i++)
+        {
+            var pathCell = Stats.PatrolPath[i];
+            var point = DungeonGridController.CellToWorldCentered(pathCell);
+
+            Gizmos.color = i == 0 ? startColor : mainColor;
+            Gizmos.DrawSphere(point, radius);
+
+            if (Stats.PatrolPingPong && i == 1)
+            {
+                Gizmos.color = startColor;
+            }
+            else if (!Stats.PatrolPingPong && i == 1)
+            {
+                Gizmos.color = startColor;
+            }
+            else
+            {
+                Gizmos.color = mainColor;
+            }
+
+            if (!Stats.PatrolPingPong || i > 0)
+            {
+                Gizmos.DrawLine(previousPoint, point);
+            }
+
+            previousPoint = point;
+        }
     }
 }
--- Assets/Scripts/EntityStats.cs
+++ Assets/Scripts/EntityStats.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [Serializable]
@@ -34,6 +35,10 @@ public class EnemyStats : EntityStats
     public int AggroRange = 4;
     [Tooltip("This cell distance + AggroRange is how far the enemy is from original aggro location before giving up chase (unless player is still in aggro range)")]
     public int ChaseRange = 2;
+    [Tooltip("The predetermined path points that patrolling entity will aim to take")]
+    public List<Vector3Int> PatrolPath = new();
+    [Tooltip("If true, patrol path will reverse once reaching the end, otherwise, reaching the end will loop to the starting point")]
+    public bool PatrolPingPong;
 
     public int LeashRange => AggroRange + ChaseRange;
 }
